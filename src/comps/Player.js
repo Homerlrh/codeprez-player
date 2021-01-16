ef64f194dc6cbae4783b4eb1288b9c67debde325
homer li <homer.lee.0508@gmail.com>
2021-01-16 10:14:37 -0800

    simple player ui

--- src/comps/Player.js
+++ src/comps/Player.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import TextEditor from "./TextEditor";
 import { ContentContext } from "../context/Provider";
 import Mp3Player from "./Mp3Player";
@@ -6,37 +6,43 @@ import Mp3Player from "./Mp3Player";
 //import db from "../db/example.json";
 const data = require("../db/example.json");
 export default function Player() {
-  //mock data
-  const content = useContext(ContentContext);
+	//mock data
+	const content = useContext(ContentContext);
 
-  const [text, setText] = useState(content.text);
-  const [timestamp, setTimestamp] = useState(content.currentTimestamp);
-  const [onPlay, setOnPlay] = useState(content.onPlay);
+	const [text, setText] = useState(content.text);
+	const [timestamp, setTimestamp] = useState(content.currentTimestamp);
+	const [onPlay, setOnPlay] = useState(content.onPlay);
 
-  const handleTextParse = (data, timestamp) => {
-    const currentContent = data.snapshots.filter(element => parseInt(element.timestamp) === timestamp)[0];
-    return currentContent.text;
-  }
-  debugger
-  useEffect(()=>{
-    if(onPlay) {
-      const interval = setInterval(() => {
-      const tx = handleTextParse(data, (timestamp+1000))
-      console.log(tx)
-      setText(tx);
-      setTimestamp(timestamp+1000);
-    }, 1000);
-    return () => clearInterval(interval);
-    }
-  })
+	const handleTextParse = (data, timestamp) => {
+		const currentContent = data.snapshots.filter(
+			(element) => parseInt(element.timestamp) === timestamp
+		)[0];
+		return currentContent.text;
+	};
+	debugger;
+	useEffect(() => {
+		if (onPlay) {
+			const interval = setInterval(() => {
+				const tx = handleTextParse(data, timestamp + 1000);
+				console.log(tx);
+				setText(tx);
+				setTimestamp(timestamp + 1000);
+			}, 1000);
+			return () => clearInterval(interval);
+		}
+	});
 
-  return(
-    <>
-      <header className="App-header">
-        <button onClick={()=> setOnPlay(!onPlay)}>Toggle Play</button>
-        <Mp3Player />
-        <TextEditor currentText={text} onPlay={onPlay}/>
-      </header>
-    </>
-  )
+	return (
+		<>
+			<div className="App-header">
+				<div style={{ width: "50%", marginRight: "5%" }}>
+					<button onClick={() => setOnPlay(!onPlay)}>Toggle Play</button>
+					<TextEditor currentText={text} onPlay={onPlay} />
+				</div>
+				<div style={{ paddingTop: "5%" }}>
+					<Mp3Player />
+				</div>
+			</div>
+		</>
+	);
 }
--- src/comps/TextEditor.js
+++ src/comps/TextEditor.js
@@ -1,44 +1,42 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Editor from "@monaco-editor/react";
 
-export default function TextEditor({currentText, onPlay, onEditing}) {
-  const [text, setText] = useState(currentText);
-  const [isEditorReady, setIsEditorReady] = useState(false);
-  
-  useEffect(() => {
-    if(onPlay)
-      setText(currentText);
-  },[currentText]);
+export default function TextEditor({ currentText, onPlay, onEditing }) {
+	const [text, setText] = useState(currentText);
+	const [isEditorReady, setIsEditorReady] = useState(false);
 
-  function handleEditorDidMount() {
-    if(!onPlay)
-      setIsEditorReady(true);
-    //valueGetter.current = _valueGetter;
-  }
+	useEffect(() => {
+		if (onPlay) setText(currentText);
+	}, [currentText]);
 
-  function handleShowValue() {
-    //alert(valueGetter.current());
-  }
+	function handleEditorDidMount() {
+		if (!onPlay) setIsEditorReady(true);
+		//valueGetter.current = _valueGetter;
+	}
 
-  const handleSaveChange = () => {
-    //TODO save code snippet into file
-  }
+	function handleShowValue() {
+		//alert(valueGetter.current());
+	}
 
-  return (
-    <>
-      <button onClick={handleShowValue} disabled={!isEditorReady}>
-        Show value
-      </button>
-      <button onClick={handleSaveChange} />
-      <Editor
-        height="90vh"
-        language="javascript"
-        readOnly={onPlay}
-        value={text}
-        editorDidMount={handleEditorDidMount}
-      />
-    </>
-  );
-  
-  
+	const handleSaveChange = () => {
+		//TODO save code snippet into file
+	};
+
+	return (
+		<>
+			<button onClick={handleShowValue} disabled={!isEditorReady}>
+				Show value
+			</button>
+			<button onClick={handleSaveChange}> Save</button>
+			<Editor
+				height="90vh"
+				width="100%"
+				language="javascript"
+				readOnly={onPlay}
+				value={text}
+				editorDidMount={handleEditorDidMount}
+				theme="vs-dark"
+			/>
+		</>
+	);
 }
